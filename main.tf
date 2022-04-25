d8da860572b6a6cbc78f9606a1e9006bc610bc01
shivakumar27342 <55353497+shivakumar27342@users.noreply.github.com>
2022-04-25 15:27:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+resource "google_project_service" "pubsub-service" {
+  project = "${var.project-id}"
+  service = "pubsub.googleapis.com"
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+}
 resource "google_pubsub_topic" "mytopic" {
-  name = "sample-topic"
+  name = "${var.topic["name"]}"
+  project = "{var.prohect-id}"
+  depends_on = ["google_project_service.pubsub-service"]
 }
