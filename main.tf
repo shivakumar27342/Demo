476d09debc3a95f8cdacc7139af6ebb3503beea3
shivakumar27342 <55353497+shivakumar27342@users.noreply.github.com>
2022-04-25 16:06:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ resource "google_pubsub_topic" "mytopic" {
   project = "{var.prohect-id}"
   depends_on = ["google_project_service.pubsub-service"]
 }
+
+resource "google_pubsub_subscription" "subscription" {
+  count = "${length(keys(var.topic["subscriptions"]))}"
+  name = "${element(keys(var.topic["subscriptions"]), count.index)}"
+  topic = "projects/${var.project-id}/topics/${var.topic["name"]}"
+  project = "${var.project-id}"
+  depends_on = ["["google_pubsub_topic.topic"]
+}
