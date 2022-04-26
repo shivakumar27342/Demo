b47bb623c0181b8eca14000fe8cc8a12ee44e1d8
shivakumar27342 <55353497+shivakumar27342@users.noreply.github.com>
2022-04-26 09:42:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 resource "google_project" "project_id" {
-  project = "${var.project-id}"
+  project = "${var.project_id}"
 }
 resource "google_pubsub_topic" "topic" {
   name = "{var.topic}"
-  project = "{var.project-id}"
+  project = "{var.project_id}"
 }
 
 # resource "google_pubsub_subscription" "subscription" {
 #   count = {length(keys(var.topic["subscriptions"]))
 #   name = element(keys(var.topic["subscriptions"]), count.index)
 #   topic = "projects/${var.project-id}/topics/${var.topic["name"]}"
-#   project = "${var.project-id}"
+#   project = "${var.project_id}"
 #   depends_on = [
 #     google_pubsub_topic.topic
 #   ]
